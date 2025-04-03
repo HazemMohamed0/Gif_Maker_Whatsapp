998a49225fde5298aa5861e3a335d0c020282781
HazemMohamed0 <hazemswefy1111@gmail.com>
2025-04-03 17:36:15 +0200

    Add whatsapp_media_editor/lib/widgets/custom_appbar.dart 1743694575170

---
+++ whatsapp_media_editor/lib/widgets/custom_appbar.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const CustomAppBar({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+      ),
+      centerTitle: true,
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Theme.of(context).colorScheme.primary,
+              Theme.of(context).colorScheme.primaryContainer,
+            ],
+          ),
+        ),
+      ),
+      elevation: 4,
+      shadowColor: Theme.of(context).shadowColor.withOpacity(0.4),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
